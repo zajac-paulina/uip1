c0f9907423a5f40dd2f9b3d3268355bf9698d85f
zajac-paulinaa <bye.zajac@gmail.com>
2024-02-10 11:43:55 +0100

    1

---
+++ server.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open('index.html', 'rb') as file:
+            self.wfile.write(file.read())
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Serwer działa na porcie {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
